e443ecb2587f4eec9e0c84bfaa174bcf278ee58c
Ben <jackfisherjack8@gmail.com>
2025-09-16 17:28:18 +0300

    feat: added views and templates

--- shop/store/views.py
+++ shop/store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+def product_list(request, slug=None):
+
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if slug:
+        category = get_object_or_404(Category, slug=slug)
+        products = products.filter(category=category)
+    
+    return render(request, 'store/product_list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/product_detail.html',{
+        'product': product
+    })
